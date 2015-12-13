eb951af1253276003c5212d4140c103c1376bcc7
AlexKVal <alexkval@gmail.com>
2015-12-13 16:39:56 +0200

    [added] "model" for "user/repo".

--- app/routes/user/repo.js
+++ app/routes/user/repo.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    let user = this.modelFor('user').login;
+
+    return $.getJSON(`https://api.github.com/repos/${user}/${params.repo_name}`);
+  }
 });
